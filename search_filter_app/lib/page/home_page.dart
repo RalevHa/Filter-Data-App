ea79957a33ff7bd3eb434cf28fefdfdf17cd2bde
Thanawat <notkaiserz@gmail.com>
2023-01-17 23:10:02 +0700

    show number onsearch product and if list empty show text for user

--- search_filter_app/lib/page/home_page.dart
+++ search_filter_app/lib/page/home_page.dart
@@ -101,109 +101,140 @@ class _HomePageState extends State<HomePage> {
         child: SafeArea(
           child: Column(
             children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(3)),
+                padding: const EdgeInsets.all(5),
+                margin: const EdgeInsets.all(3),
+                child: GetX<CaseController>(
+                  builder: (controller) {
+                    return Text(
+                      'ขณะสินค้าแสดงผลอยู่ ${caseController.displaylist.length} รายการ',
+                      style: const TextStyle(fontSize: 18),
+                    );
+                  },
+                ),
+              ),
               Expanded(
                 child: GetX<CaseController>(
                   builder: (controller) {
-                    return ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: controller.displaylist.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          margin: const EdgeInsets.all(12),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: <Widget>[
-                                Center(
-                                  child: CachedNetworkImage(
-                                    imageUrl:
-                                        caseController.displaylist[index].image,
-                                    errorWidget: (context, url, error) =>
-                                        const Icon(Icons.error),
-                                    progressIndicatorBuilder:
-                                        (context, url, downloadProgress) =>
-                                            Shimmer.fromColors(
-                                      highlightColor: Colors.white,
-                                      baseColor: Colors.grey.shade300,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.grey,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(5.0)),
-                                        ),
-                                        child: ClipRRect(
-                                          borderRadius: const BorderRadius.all(
-                                              Radius.circular(10.0)),
-                                          child: AspectRatio(
-                                            aspectRatio: 1 / 1,
+                    return (caseController.displaylist.isEmpty)
+                        ? Center(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10)),
+                                padding: const EdgeInsets.all(5),
+                                margin: const EdgeInsets.all(3),
+                                child: const Text(
+                                  'ไม่พบสินค้าที่ท่านกำลังค้นหา\nกรุณาลองใหม่อีกครั้ง',
+                                  style: TextStyle(fontSize: 26),
+                                  textAlign: TextAlign.center,
+                                )),
+                          )
+                        : ListView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: controller.displaylist.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Card(
+                                margin: const EdgeInsets.all(12),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: <Widget>[
+                                      Center(
+                                        child: CachedNetworkImage(
+                                          imageUrl: caseController
+                                              .displaylist[index].image,
+                                          errorWidget: (context, url, error) =>
+                                              const Icon(Icons.error),
+                                          progressIndicatorBuilder: (context,
+                                                  url, downloadProgress) =>
+                                              Shimmer.fromColors(
+                                            highlightColor: Colors.white,
+                                            baseColor: Colors.grey.shade300,
                                             child: Container(
-                                              color: Colors.grey,
+                                              decoration: const BoxDecoration(
+                                                color: Colors.grey,
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(5.0)),
+                                              ),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    const BorderRadius.all(
+                                                        Radius.circular(10.0)),
+                                                child: AspectRatio(
+                                                  aspectRatio: 1 / 1,
+                                                  child: Container(
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                              ),
                                             ),
                                           ),
+                                          fit: BoxFit.cover,
+                                          width: double.infinity,
                                         ),
                                       ),
-                                    ),
-                                    fit: BoxFit.cover,
-                                    width: double.infinity,
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              SizedBox(
+                                                width: size.width * 0.6,
+                                                child: Text(
+                                                  caseController
+                                                      .displaylist[index].title,
+                                                  style: const TextStyle(
+                                                      fontSize: 20),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: size.width * 0.6,
+                                                child: Text(
+                                                  'Brand: ${caseController.displaylist[index].brand}',
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: size.width * 0.6,
+                                                child: Text(
+                                                  'For: ${caseController.displaylist[index].version}',
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: size.width * 0.6,
+                                                child: Text(
+                                                  'Color: ${caseController.displaylist[index].color}',
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Text(
+                                            '${caseController.displaylist[index].price} ฿',
+                                            style:
+                                                const TextStyle(fontSize: 24),
+                                          ),
+                                        ],
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          cartController.addToCard(
+                                              controller.displaylist[index]);
+                                        },
+                                        child: const Text('Add to Cart'),
+                                      )
+                                    ],
                                   ),
                                 ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        SizedBox(
-                                          width: size.width * 0.6,
-                                          child: Text(
-                                            caseController
-                                                .displaylist[index].title,
-                                            style:
-                                                const TextStyle(fontSize: 20),
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: size.width * 0.6,
-                                          child: Text(
-                                            'Brand: ${caseController.displaylist[index].brand}',
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: size.width * 0.6,
-                                          child: Text(
-                                            'For: ${caseController.displaylist[index].version}',
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: size.width * 0.6,
-                                          child: Text(
-                                            'Color: ${caseController.displaylist[index].color}',
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Text(
-                                      '${caseController.displaylist[index].price} ฿',
-                                      style: const TextStyle(fontSize: 24),
-                                    ),
-                                  ],
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    cartController.addToCard(
-                                        controller.displaylist[index]);
-                                  },
-                                  child: const Text('Add to Cart'),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    );
+                              );
+                            },
+                          );
                   },
                 ),
               ),
